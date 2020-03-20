62148e3f140bc7f584b488556f53fe674f8c02a7
Terrence Bell <terrencekbell@gmail.com>
2020-03-20 03:53:26 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+  @@all = []
+  attr_accessor :date, :patient, :doctor 
+def initialize(patient, date, doctor)
+  @patient = patient
+  @doctor = doctor
+  @date = date 
+  @all << self 
+end
+def self.all 
+  @@all 
+end 
+end 
---
+++ lib/doctor.rb
@@ -0,0 +1,14 @@
+class Doctor 
+  @@all = []
+  attr_accessor :appointment, :patient, :name 
+  def initialize(name)
+    @name = name 
+    @@all << self
+    end
+    def self.all 
+      @@all 
+    end 
+    def appointments
+      
+    end 
+end
