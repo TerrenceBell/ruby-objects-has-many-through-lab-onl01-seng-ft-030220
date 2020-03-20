3129ddfff12354d3de5a707d671d0e8f15958782
Terrence Bell <terrencekbell@gmail.com>
2020-03-20 04:05:59 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,13 @@
 class Appointment
   @@all = []
   attr_accessor :date, :patient, :doctor 
-def initialize(date, doctor)
-    @date = date
-    @doctor = doctor
-    doctor.add_appointment(self) 
+def initialize(date, patient, doctor)
+  @patient = patient
+  @doctor = doctor
+  @date = date 
+  @all << self 
 end
-
+def self.all 
+  @@all 
+end 
 end 
