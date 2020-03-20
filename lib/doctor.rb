8776f46f1621495e79b20b08c553ad962e3a83fd
Terrence Bell <terrencekbell@gmail.com>
2020-03-20 04:48:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,6 @@ class Doctor
       
     end 
     def appointments 
-      Appointment.all.select do |apt|
-        apt.doctor == self 
-      end 
-    end 
+      Appointment.all.select{|apt| apt.doctor == self}
+    end
 end
